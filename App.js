14fcd83b049ba3fde12c5945fe84ffe0958e1352
Juan Pablo Arroyave <50724493+JuanPh2002@users.noreply.github.com>
2021-09-26 22:19:06 -0500

    Component for CarItem

--- App.js
+++ App.js
@@ -2,16 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, SafeAreaView, View, Text, ImageBackground} from "react-native";
 
+//Component CarItem
+import CarItem from "./components/CarItem";
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.carContainer}>
-        <ImageBackground source={require("./assets/images/ModelS.jpeg")} style={styles.image}/>
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,860</Text>
-        </View>
-      </View>
+      <CarItem/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -23,29 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  carContainer: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#fff",
-  },
-  titles:{
-    marginTop: "25%",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "500",
-    color: "#000"
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#5C5E62"
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-    position: "absolute"
   },
 });
